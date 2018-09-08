5ecf40b75bcfcd179cc128c156dd2691d00ff7be
Mike DeVine <pixelSHREDDER@gmail.com>
2018-09-08 15:53:55 -0700

    Added reviewzine child theme files

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+// jshint node:true
+
+module.exports = function( grunt ) {
+    'use strict';
+
+    var loader = require( 'load-project-config' ),
+        config = require( 'grunt-theme-fleet' );
+    config = config();
+    config.files.php.push( '!inc/admin/**/*.php' );
+    config.files.php.push( '!class-tgm-plugin-activation.php' );
+    config.files.js.push( '!inc/admin/**/*.js' );
+    loader( grunt, config ).init();
+};
